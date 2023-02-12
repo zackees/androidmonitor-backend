c85d41a9e4e377fce9e9bdba3e368f3c4f3d9e95
zackees <z@zackees.com>
2023-02-11 23:16:43 -0800

    adds -f to push.sh

--- push.sh
+++ push.sh
@@ -1,3 +1,19 @@
 #! /bin/bash
-git push
-git push --mirror https://github.com/zackees/androidmonitor-backend
+set -e
+# extract -f from the command line
+# https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -f|--force)
+    FORCE=-f
+    shift # past argument
+    ;;
+    *)    # unknown option
+    shift # past argument
+    ;;
+esac
+done
+git push $FORCE --mirror
+git push $FORCE --mirror https://github.com/zackees/androidmonitor-backend
