4223c48c98a4c16a070aacb84df8c241fdca2589
zackees <z@zackees.com>
2023-04-06 14:00:10 -0700

    adds stub test_endpoints.py

---
+++ tests/test_endpoints.py
@@ -0,0 +1,17 @@
+"""
+Tests the endpoints of the API.
+"""
+
+import unittest
+
+
+class EndpointTester(unittest.TestCase):
+    """Test harness for the endpoints of the API."""
+
+    def test_getinfo(self) -> None:
+        """TODO -  this should test the /v1/getinfo endpoint."""
+        return
+
+
+if __name__ == "__main__":
+    unittest.main()
