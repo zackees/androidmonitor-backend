9fd9310834efa294b54bb74a7565fa6b1330efd1
zackees <zachvorhies@protonmail.com>
2023-04-06 22:26:55 -0700

    fixes environment for mac: python alias to python3 in activate

--- activate.sh
+++ activate.sh
@@ -1,20 +1,21 @@
-
 #!/bin/bash
 set -e
-function abs_path {
-  (cd "$(dirname '$1')" &>/dev/null && printf "%s/%s" "$PWD" "${1##*/}")
-}
-
-# if make_venv dir is not present, then make it
-if [ ! -d "venv" ]; then
-  python make_venv.py
-fi
 # if IN_ACTIVATED_ENV is set, then we are already in the venv
 if [ -n "$IN_ACTIVATED_ENV" ]; then
   return
 fi
+function abs_path {
+  (cd "$(dirname '$1')" &>/dev/null && printf "%s/%s" "$PWD" "${1##*/}")
+}
+# if python is not python3, then make it
+if [ "$(python --version 2>&1)" != "Python 3"* ]; then
+  alias python=python3
+  alias pip=pip3
+fi
+# if make_venv dir is not present, then make it
+if [ ! -d "venv" ]; then
+  python make_venv.py
+fi
 . $( dirname $(abs_path ${BASH_SOURCE[0]}))/venv/bin/activate
 export PATH=$( dirname $(abs_path ${BASH_SOURCE[0]}))/:$PATH
-alias python3=python
-alias pip3=pip
 export IN_ACTIVATED_ENV="1"
